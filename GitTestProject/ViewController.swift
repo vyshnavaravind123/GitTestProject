a035e7511fd205fe7162022fbc0582f15b721251
Vyshnav <vyshnav@kawikatechnologies.com>
2018-02-21 18:39:21 +0530

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by VYSHNAV ARAVIND on 2/21/18.
+//  Copyright © 2018 VYSHNAV ARAVIND. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
